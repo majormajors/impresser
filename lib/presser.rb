fde21431a60134e3863adca3c3e8b5d319d61075
Matt Mayers <matt@mattmayers.com>
2010-09-29 23:45:59 -0400

    * unify config options into an OpenStruct * might switch this to a hash if i decide ostruct won't work

--- lib/presser.rb
+++ lib/presser.rb
@@ -1,6 +1,9 @@
 $: << File.expand_path(File.dirname(__FILE__))
 
 module Presser
+  Config = OpenStruct.new(
+    :table_prefix => "wp_",
+    :wp_content_path => nil)
 end
 
 require "presser/model"
--- lib/presser/model/active_record.rb
+++ lib/presser/model/active_record.rb
@@ -1,20 +1,6 @@
 module Presser
   class Base < ::ActiveRecord::Base
     self.abstract_class = true
-
-    class << self
-      def wp_content_path=(path)
-        write_inheritable_attribute(:wp_content_path, path)
-      end
-      
-      def wp_content_path
-        read_inheritable_attribute(:wp_content_path)
-      end
-      
-      def table_name_prefix
-        ENV["PRESSER_TABLE_PREFIX"] || "wp_"
-      end
-    end
   end
   
   def self.load_models
@@ -30,6 +16,6 @@ module Presser
     require "presser/model/active_record/term_relationship"
     require "presser/model/active_record/term_taxonomy"
     require "presser/model/active_record/user"
-    require "presser/model/active_record/user_meta" 
+    require "presser/model/active_record/user_meta"
   end
 end
