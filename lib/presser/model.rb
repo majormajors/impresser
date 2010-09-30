027286e3cb38f6a4707c8a2dfb7e4bfa0baaf29f
Matt Mayers <matt@mattmayers.com>
2010-09-30 01:21:17 -0400

    * just documenting a method

--- lib/presser/model.rb
+++ lib/presser/model.rb
@@ -1,6 +1,9 @@
 module Presser
   Models = %w(comment comment_meta link option post_base post page post_meta term term_relationship term_taxonomy user user_meta).freeze
   
+  # Loads data models for the specified ORM
+  #
+  # @param [String] orm The ORM for which you want to load models
   def self.load_models_for(orm)
     Models.each{ |model| require File.join("presser","model", orm, model) }
   end
