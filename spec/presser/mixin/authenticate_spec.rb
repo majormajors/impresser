7aafeafc65b13fc6864e6d321aa9a55e4cc2ccea
Matt Mayers <matt@mattmayers.com>
2012-03-17 19:02:15 -0700

    Added specs for Authenticate module
    It looks like the phpass-ruby gem doesn't handle the new, more secure
    passwords generated by phpass. I'll need to figure out if Wordpress uses
    these and potentially patch it.

--- spec/presser/mixin/authenticate_spec.rb
+++ spec/presser/mixin/authenticate_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 require 'presser/mixin/authenticate'
 
 describe Presser::Authenticate do
+  before(:each) do
+    hashed_password = '$P$BkgreYPfZOYNFX.V9IdMLcaBampElR1'
+    @user = mock('User', :login => 'gooduser', :hashed_password => hashed_password)
+    @good_password = 'abc123'
+    @bad_password = 'abc456'
+  end
+
   before(:all) do
     @klass = Class.new
     @klass.__send__(:include, Presser::Authenticate)
@@ -13,18 +20,27 @@ describe Presser::Authenticate do
     end
   end
 
-  context "find_user_for_authentication has not been implemented" do
-    before(:all) do
-      _self = self
-      class << @klass
-        def find_user_for_authentication(login)
-          _self.mock("User")
-        end
+  context "find_user_for_authentication has been implemented" do
+    before(:each) do
+      @klass.stub(:find_user_for_authentication) do |login|
+        login == 'gooduser' ? @user : nil
       end
     end
 
     describe ".authenticate" do
       it{ lambda{ @klass.authenticate('abc', '123') }.should_not raise_error(NotImplementedError) }
+
+      it "returns the user object when the username and password are good" do
+        @klass.authenticate('gooduser', @good_password).should == @user
+      end
+
+      it "returns nil when the username is bad" do
+        @klass.authenticate('baduser', @good_password).should be_nil
+      end
+
+      it "returns nil when the password is bad but the username is good" do
+        @klass.authenticate('gooduser', @bad_password).should be_nil
+      end
     end
   end
 end
