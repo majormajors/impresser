102008f6708f1477666ec397b6177af48f15fee1
Matt Mayers <matt@mattmayers.com>
2012-03-18 20:10:06 -0700

    Added support for the composite primary key on the TermRelationship model

--- lib/impresser/active_record/term_relationship.rb
+++ lib/impresser/active_record/term_relationship.rb
@@ -1,7 +1,10 @@
+require 'composite_primary_keys'
+
 module Impresser
   module ActiveRecord
     class TermRelationship < Base
       self.table_name = with_prefix("term_relationships")
+      self.primary_keys = :object_id, :term_taxonomy_id
       belongs_to :term_taxonomy
       belongs_to :object, :class_name => "PostBase", :foreign_key => "object_id"
     end
